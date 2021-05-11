6980561a4751fb23aa76c22d8a9c205c8c03a8b4
Ricardo Carvalho <rapc99@ua.pt>
2021-05-11 02:52:17 +0100

    RabbitMQ cluster and Redis replication (WIP)

--- docker_images/redis/docker-entrypoint.sh
+++ docker_images/redis/docker-entrypoint.sh
@@ -1,16 +1,25 @@
 #!/bin/sh
 set -e
+set -x
 
-# first arg is `-f` or `--some-option`
-# or first arg is `something.conf`
-if [ "${1#-}" != "$1" ] || [ "${1%.conf}" != "$1" ]; then
-	set -- redis-server "$@"
+echo "bind 0.0.0.0" > /etc/redis.conf
+echo "protected-mode no" >> /etc/redis.conf
+
+set -- $(which redis-server) /etc/redis.conf
+
+if [ -v REPLICA ] && [ ! -v SENTINEL ]; then
+  set -- $@ --slaveof $(redis-cli -h redis-sentinel -p 26379 --raw sentinel get-master-addr-by-name primary | sed -n 1p) 6379
 fi
 
-# allow the container to be started with `--user`
-if [ "$1" = 'redis-server' -a "$(id -u)" = '0' ]; then
-	find . \! -user redis -exec chown redis '{}' +
-	exec su-exec redis "$0" "$@"
+if [ -v SENTINEL ]; then
+	echo "sentinel monitor primary $SENTINEL 6379 2" >> /etc/redis.conf
+	echo "sentinel down-after-milliseconds primary 5000" >> /etc/redis.conf
+	echo "sentinel failover-timeout primary 10000" >> /etc/redis.conf
+	echo "sentinel parallel-syncs primary 1" >> /etc/redis.conf
+
+	set -- $@ --port 26379 --sentinel
 fi
+
+cat /etc/redis.conf
 
 exec "$@"
